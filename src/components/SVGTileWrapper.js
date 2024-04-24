19dd6020b4c0705192297285b8b8a753e325c297
Stephen Brooks <osldgoth@gmail.com>
2024-04-23 23:35:43 -0400

    returning to using component in switch statement and null for default case

--- src/components/SVGTileWrapper.js
+++ src/components/SVGTileWrapper.js
@@ -17,12 +17,11 @@ const SVGTileWrapper = () => {
     console.log(event.target.value)
   }
 
-let pathD="M 10 10 L 30 30 T 60 10 90 30 L 100 100 T 70 70 30 70 10 50 Z"
-pathD += `M 105 105 h ${Math.floor(Math.random()*100)} v ${Math.floor(Math.random()*100)} Z`
-console.log(pathD)
+// let pathD="M 10 10 L 30 30 T 60 10 90 30 L 100 100 T 70 70 30 70 10 50 Z"
+// pathD += `M 105 105 h ${Math.floor(Math.random()*100)} v ${Math.floor(Math.random()*100)} Z`
+// console.log(pathD)
   return (
     <div>
-      
       <SVGForm handleShapeChange={handleShapeChange} handleFormSubmission={handleFormSubmission}/>
       {allSVGs.map((currentSVG, id) => {
         return (
@@ -31,9 +30,9 @@ console.log(pathD)
       })}    
       
       
-      <svg width="378" height="378" style={{border: "solid red 1px"}}>
+      {/* <svg width="378" height="378" style={{border: "solid red 1px"}}>
   <path d={pathD} fill="none" stroke='yellow' strokeDasharray='5'/>
-</svg>
+</svg> */}
 
     </div>
   )
--- src/components/ShapeSwitch.js
+++ src/components/ShapeSwitch.js
@@ -7,38 +7,13 @@ const Shape = (props) => {
   const {shape} = props
   switch (shape) {
     case RECT:
-      return     (<div>
-      <label>X</label>
-      <input type='number' />
-      <label>Y</label>
-      <input type='number' />
-      <label>Width</label>
-      <input type='number' />
-      <label>Height</label>
-      <input type='number' />
-      <label>rx</label>
-      <input type='number' />
-      <label>ry</label>
-      <input type='number' />
-    </div>
-      )
-      // <RectAttributes />
+      return <RectAttributes />
     default:
-      return (<div>
-        <label>X</label>
-        <input type='number' />
-        <label>Y</label>
-        <input type='number' />
-        <label>Width</label>
-        <input type='number' />
-        <label>Height</label>
-        <input type='number' />
-        <label>rx</label>
-        <input type='number' />
-        <label>ry</label>
-        <input type='number' />
+      return (
+      <div>
+        null
       </div>
-        )
+      )
   }
 }
 
