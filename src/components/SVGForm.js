0b9bd16439f4e3cb0ba1ca9f456d033bd8713c39
Stephen Brooks <osldgoth@gmail.com>
2024-04-24 16:46:07 -0400

    removed comments, completed options for select.

--- src/components/SVGForm.js
+++ src/components/SVGForm.js
@@ -2,45 +2,22 @@ import React, {useContext} from 'react'
 import ShapeSwitch from './ShapeSwitch'
 import { Context } from "./SVGContext"
 
-const SVGForm = ({handleFormSubmission, handleShapeChange}) => {
+const SVGForm = ({uuidv4, handleFormSubmission, handleShapeChange}) => {
   const {SVG} = useContext(Context)
   const shapes = ["Rect", "Circle", 'Ellipse', 'Line', 'Polyline', 'Polygon', 'Path']
   return (
     <form className='svgForm' onSubmit={handleFormSubmission}>
-        <select name="" id="" onChange={handleShapeChange}>
+        <select name="" id="" onChange={handleShapeChange} defaultValue="none">
+          <option id={uuidv4()} key={uuidv4()} value="none" disabled>Select a shape</option>
           {shapes.map((shape) => {
             return (
-            <option id={shape} value={shape === "Rectangle" ? "rect" : shape.toLowerCase()}>
+            <option id={uuidv4()} key={uuidv4()} value={shape === "Rectangle" ? "rect" : shape.toLowerCase()}>
               {shape} 
             </option>)
           })}
-          
-          {/* 
-          
-          */}
-            {/* 
-              M x y; 
-              L x y; 
-              T x y; 
-              H x; 
-              V y; 
-              C x1 y1, x2 y2, x y;
-              S x2 y2, x y; 
-              Q x1 y1, x y; 
-              A rx ry x-axis-rotation large-arc-flag sweep-flag x y
-              Z; 
-            */}
-            {/* 
-              XY *7
-              X  *1
-              Y  *1
-              X1,X2,Y1,Y2 *2
-              RX, RY, *1
-            */}
         </select>
-        {/* conditional */}
-        
-          <ShapeSwitch shape={SVG}/>
+        console.log(shape.name)
+        <ShapeSwitch shape={SVG.name}/>
         
         {/* for path */}
         <select>
@@ -50,7 +27,7 @@ const SVGForm = ({handleFormSubmission, handleShapeChange}) => {
           <option value={"M"}>C</option>
           <option value={"M"}>T</option>
           <option value={"M"}>S</option>
-          {/* ... */}
+        
         </select>
         {/* stroke, fill, stroke-width */}
         <button type='submit' className='submit-svg-form'>Create SVG</button>
