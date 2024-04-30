a433f2dd7fc938fd717a04f8f9aac7369ef8f891
Stephen Brooks <osldgoth@gmail.com>
2024-04-30 00:22:09 -0400

    added some BS icons for editing and deleting svgs

--- src/components/SVGTile.js
+++ src/components/SVGTile.js
@@ -4,9 +4,15 @@ import React from 'react'
 const SVGTile = ({ currentSVG }) => {
   const { svg, shapeName, attributes } = currentSVG;
 
+
+
   return (
-    <div className="svg-tile">
-      <h2>{shapeName}</h2>
+    <div className="card">
+      <div className="card-header">
+        <h2>{shapeName}</h2>
+        <i className="bi bi-x-octagon"></i>
+        <i className="bi bi-pencil-square"></i>
+      </div>
       {svg}
     </div>
   );
