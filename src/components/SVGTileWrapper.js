12e156f15e721f0d7e119c611bc1e6e4a21ed2aa
Stephen Brooks <osldgoth@gmail.com>
2024-04-30 00:21:34 -0400

    new svgs are added to the beginning of the state array now

--- src/components/SVGTileWrapper.js
+++ src/components/SVGTileWrapper.js
@@ -8,12 +8,13 @@ uuidv4()
 
 const SVGTileWrapper = () => {
   const {shapeName, setShapeName, allSVGs, setAllSVGs, attributes} = useContext(Context)
+  
   const handleFormSubmission = (event) => {
     event.preventDefault()
     const shape = React.createElement(shapeName, {...attributes, fill: "none", strokeWidth: 1, stroke: "red" })
     const svg = React.createElement('svg', {id: uuidv4(), key: uuidv4(), width: 378, height: 378, style:{border: "solid red 1px"}}, shape)
-  
-    setAllSVGs([...allSVGs, {svg, shapeName, attributes}]) 
+  //clear shapeName and attributes?
+    setAllSVGs([{svg, shapeName, attributes}, ...allSVGs]) //add new svg to beginning of array
   }
 
   const handleShapeChange = (event) => {
@@ -21,12 +22,14 @@ const SVGTileWrapper = () => {
     setShapeName(event.target.value)
   }
 
+  const allSVGsReversed = allSVGs.reverse()
+
   return (
     <div>
-      <h1> basic site info - Not an exhaustive SVG builder</h1>
+      <h1>The following is not an exhaustive SVG builder, but rather a very basic and rudimentary one.</h1>
       <SVGForm uuidv4={uuidv4} handleShapeChange={handleShapeChange} handleFormSubmission={handleFormSubmission}/>
 
-      {allSVGs.map(currentSVG => {
+      {allSVGsReversed.map(currentSVG => {
         return (
           <SVGTile id={uuidv4()} key={uuidv4()} currentSVG={currentSVG} />
         )
