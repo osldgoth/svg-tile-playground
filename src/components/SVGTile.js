87ebf6d202cce1fe89f9537a4f17221cc7fc2e29
Stephen Brooks <osldgoth@gmail.com>
2024-05-20 08:42:56 -0400

    create svg in tile component, conditional delete and edit icons

--- src/components/SVGTile.js
+++ src/components/SVGTile.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useContext} from 'react'
+import { Context } from "./SVGContext"
+import {v4 as uuidv4} from 'uuid'
 
-const SVGTile = ({index, uuidv4, currentSVG , handleDelete, handleEdit}) => {
-  const { svg, shapeName, attributes } = currentSVG;
 
 
+const SVGTile = ({index, shapeName, attributes, handleDelete, handleEdit}) => {
+  // const {currentSVG, setCurrentSVG, shapeName, setAllSVGs, attributes, allSVGs, setShapeName, setAttributes} = useContext(Context)
+  let shape = null;
+  if (typeof shapeName === 'string' && shapeName.trim() !== '') {
+    shape = React.createElement(shapeName, {...attributes, fill: "none", strokeWidth: 1, stroke: "red" })
+  }
+  const currentsvg = React.createElement('svg', {id: uuidv4(), key: uuidv4(), width: 378, height: 378, style:{border: "solid green 1px"}}, shape)
 
   return (
-    <div className="container">
-      <div className="card" id={uuidv4()} key={uuidv4()} >
+    <div className="container" id={uuidv4()} key={uuidv4()}>
+      <div className="card">
         <div className="card-header">
           <h2>{shapeName}</h2>
-          <i className="bi bi-pencil-square" data-index={index} onClick={handleEdit}></i>
-          <i className="bi bi-x-octagon" data-index={index} onClick={handleDelete}></i>
+          {handleDelete ? <i className="bi bi-pencil-square" data-index={index} onClick={handleEdit}></i> : <></>}
+          {handleEdit ? <i className="bi bi-x-octagon" data-index={index} onClick={handleDelete}></i> : <></>}
         </div>
         <div className="card-body">
-          {svg}
+          {currentsvg}
         </div>
       </div>
     </div>
