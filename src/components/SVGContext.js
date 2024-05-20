221c3360223d3c794e6bbfd041bb87caeb8f4380
Stephen Brooks <osldgoth@gmail.com>
2024-05-20 08:41:49 -0400

    default currentSVG state

--- src/components/SVGContext.js
+++ src/components/SVGContext.js
@@ -1,11 +1,16 @@
 import React, {useState, createContext} from 'react'
+import {v4 as uuidv4} from 'uuid'
+
 const Context = createContext()
 
 const SVGContextProvider = (props) => {
   const [shapeName, setShapeName] = useState("")
   const [allSVGs, setAllSVGs] = useState([])
   const [attributes, setAttributes] = useState({})
-  const [currentSVG, setCurrentSVG] = useState(null)
+  const [currentSVG, setCurrentSVG] = useState(React.createElement(
+    'svg', 
+    {id: uuidv4(), key: uuidv4(), width: 378, height: 378, style:{border: "solid green 1px"}}
+  ))
 
   return (
     <Context.Provider value={{shapeName, setShapeName, currentSVG, setCurrentSVG, allSVGs, setAllSVGs, attributes, setAttributes}}>
