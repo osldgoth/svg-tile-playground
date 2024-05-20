dd68af495d603b7cb4904c47cb4c15c421236e8c
Stephen Brooks <osldgoth@gmail.com>
2024-05-20 08:40:31 -0400

    import uuid, component name change, added import of tile, display tile instead of currentSVG

--- src/components/SVGForm.js
+++ src/components/SVGForm.js
@@ -1,48 +1,47 @@
 import React, {useContext} from 'react'
-import ShapeSwitch from './ShapeSwitch'
+import ShapeInputs from './ShapeInputs'
 import { Context } from "./SVGContext"
+import {v4 as uuidv4} from 'uuid'
+import SVGTile from './SVGTile'
 
-const SVGForm = ({uuidv4}) => {
+
+const SVGForm = () => {
   const {currentSVG, setCurrentSVG, shapeName, setAllSVGs, attributes, allSVGs, setShapeName, setAttributes} = useContext(Context)
   const shapes = ["Rectangle", "Circle", 'Ellipse', 'Line', 'Polyline', 'Polygon', 'Path']
 
   const handleFormSubmission = (event) => {
     event.preventDefault()
   
-    setAllSVGs([{currentSVG, shapeName, attributes}, ...allSVGs]) //add new svg to beginning of array
+    setAllSVGs([{shapeName, attributes}, ...allSVGs]) //add new svg to beginning of array
     //clear shapeName and attributes
     setShapeName("")
     setAttributes({})
   }
 
   const handleShapeChange = (event) => {
-    event.preventDefault()
-    
+    //event.preventDefault()
+
     setShapeName(event.target.value)
     setAttributes({})
-    
-    const shape = React.createElement(event.target.value, {...attributes, fill: "none", strokeWidth: 1, stroke: "red" })
-    const svg = React.createElement('svg', {id: uuidv4(), key: uuidv4(), width: 378, height: 378, style:{border: "solid red 1px"}}, shape)
-    setCurrentSVG(svg)
   }
   
   return (
     <form id={uuidv4()} className='svgForm' onSubmit={handleFormSubmission}>
-        <select className='mb-3' name="" id="shapeSelection" onChange={handleShapeChange} value={shapeName}>
-          <option id={uuidv4()} key={uuidv4()} value="" disabled>Select a shape</option>
-          {shapes.map((shape) => {
-            return (
-            <option id={uuidv4()} key={uuidv4()} value={shape === "Rectangle" ? "rect" : shape.toLowerCase()}>
-              {shape} 
-            </option>)
-          })}
-        </select>
+      <select className='mb-3' name="" id="shapeSelection" onChange={handleShapeChange} value={shapeName}>
+        <option id={uuidv4()} key={uuidv4()} value="" disabled>Select a shape</option>
+        {shapes.map((shape) => {
+          return (
+          <option id={uuidv4()} key={uuidv4()} value={shape === "Rectangle" ? "rect" : shape.toLowerCase()}>
+            {shape} 
+          </option>)
+        })}
+      </select>
 
-        <ShapeSwitch shape={shapeName}/>
-        {currentSVG}
-        <button type='submit' className='submit-svg-form my-3'>Create SVG</button>
-        
-      </form>
+      <ShapeInputs shape={shapeName.toUpperCase()}/>
+      <SVGTile index={-1} shapeName={shapeName} attributes={attributes} /> {/* handleDelete={handleDelete} handleEdit={handleEdit} */}
+      <button type='submit' className='submit-svg-form my-3'>Create SVG</button>
+      
+    </form>
       
   )
 }
