2638ec4a837724f9a29304c06be537d598eff694
Stephen Brooks <osldgoth@gmail.com>
2024-05-30 11:06:25 -0400

    advShape data and logic

--- src/components/ShapeInputs.js
+++ src/components/ShapeInputs.js
@@ -1,84 +1,271 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useContext} from 'react';
 import  LabelInput from './LabelInput'
+import { Context } from './SVGContext';
 
 
-const shapeConfig = {
+const basicShapeConfig = {
   RECT: [
-    { parameter: "x", label: "X Coordinate", type: "number" },
-    { parameter: "y", label: "Y Coordinate", type: "number" },
-    { parameter: "height", label: "Height", type: "number" },
-    { parameter: "width", label: "Width", type: "number" },
-    { parameter: "rx", label: "X Radius", type: "number" },
-    { parameter: "ry", label: "Y Radius", type: "number" },
+    { parameter: "x", label: "X Coordinate" },
+    { parameter: "y", label: "Y Coordinate" },
+    { parameter: "height", label: "Height" },
+    { parameter: "width", label: "Width" },
+    { parameter: "rx", label: "X Radius" },
+    { parameter: "ry", label: "Y Radius" },
   ],
   CIRCLE: [
-    { parameter: "r", label: "Radius", type: "number" },
-    { parameter: "cx", label: "Center X", type: "number" },
-    { parameter: "cy", label: "Center Y", type: "number" },
+    { parameter: "r", label: "Radius" },
+    { parameter: "cx", label: "Center X" },
+    { parameter: "cy", label: "Center Y" },
   ],
   ELLIPSE: [
-    { parameter: "rx", label: "Radius X", type: "number" },
-    { parameter: "ry", label: "Radius Y", type: "number" },
-    { parameter: "cx", label: "Center X", type: "number" },
-    { parameter: "cy", label: "Center Y", type: "number" },
+    { parameter: "rx", label: "Radius X" },
+    { parameter: "ry", label: "Radius Y" },
+    { parameter: "cx", label: "Center X" },
+    { parameter: "cy", label: "Center Y" },
   ],
   LINE: [
-    { parameter: "x1", label: "X1", type: "number" },
-    { parameter: "y1", label: "Y1", type: "number" },
-    { parameter: "x2", label: "X2", type: "number" },
-    { parameter: "y2", label: "Y2", type: "number" },
-  ],
-  POLYLINE: [
-    { parameter: "points", label: "Points", type: "text" },
-  ],
-  POLYGON: [
-    { parameter: "points", label: "Points", type: "text" },
-  ],
-  PATH: [
-    { parameter: "d", label: "Path Data", type: "text" },
+    { parameter: "x1", label: "X1" },
+    { parameter: "y1", label: "Y1" },
+    { parameter: "x2", label: "X2" },
+    { parameter: "y2", label: "Y2" },
   ],
 };
 
+const advancedShapeConfig = {
+  POLYLINE: [
+    { 
+      attribute: "points=",
+      label: "A list of points.",
+      parameters: [
+        { parameter: 'x', label: "X Coordinate" },
+        { parameter: 'y', label: "Y Coordinate" }
+      ]
+    }
+  ],
+  POLYGON: [
+    { 
+      attribute: "points=",
+      label: "A list of points.",
+      parameters: [
+        { parameter: 'x', label: "X Coordinate" },
+        { parameter: 'y', label: "Y Coordinate" }
+      ]
+    }
+  ],
+  PATH: [
+    { 
+      attribute: "d=",
+      label: "Path Data.",
+      commands: [ 
+        {
+          command: 'M', 
+          name: "Move",
+          parameters: [
+            { parameter: 'x', label: "X Coordinate" },
+            { parameter: 'y', label: "Y Coordinate" }
+          ]
+        },
+        {
+          command: 'L', 
+          name: "Line To",
+          parameters: [
+            { parameter:  'x', label: "X Coordinate" },
+            { parameter:  'y', label: "Y Coordinate" }
+          ]
+        },
+        {
+          command: 'H', 
+          name: "Horizontal line",
+          parameters: [
+            { parameter: 'x', label: "X Coordinate" }
+          ]
+        },
+        {
+          command: 'V', 
+          name: "Vertical line",
+          parameters: [
+            { parameter: 'y', label: "Y Coordinate" }
+          ]
+        },
+        {
+          command: 'C', 
+          name: "Cubic Béziers",
+          parameters: [
+            { parameter: 'x1', label: "X1" },
+            { parameter: 'y1', label: "Y1" },
+            { parameter: 'x2', label: "X2" },
+            { parameter: 'y2', label: "Y2" },
+            { parameter: 'x', label: "X Coordinate" },
+            { parameter: 'y', label: "Y Coordinate" }
+          ]
+        },
+        {
+          command: 'S', 
+          name: "Smooth Cubic Béziers",
+          parameters: [
+            { parameter: 'x2', label: "X2" },
+            { parameter: 'y2', label: "Y2" },
+            { parameter: 'x', label: "X Coordinate" },
+            { parameter: 'y', label: "Y Coordinate" }
+          ]
+        },
+        {
+          command: 'Q', 
+          name: "Quadratic Bézier",
+          parameters: [
+            { parameter: 'x1', label: "X1" },
+            { parameter: 'y1', label: "Y1" },
+            { parameter: 'x', label: "X Coordinate" },
+            { parameter: 'y', label: "Y Coordinate" }
+          ]
+        },
+        {
+          command: 'T', 
+          name: "Smooth Quadratic Bézier",
+          parameters: [
+            { parameter: 'x', label: "X Coordinate" },
+            { parameter: 'y', label: "Y Coordinate" }
+          ]
+        },
+        {
+          command: 'A', 
+          name: "Arc",
+          parameters: [
+            { parameter: 'rx', label: "Radius X" }, 
+            { parameter: 'ry', label: "Radius Y" },
+            { parameter: 'x-axis-rotation(0-360)', label: "X Axis Rotation" },
+            { parameter: 'x', label: "X Coordinate" },
+            { parameter: 'y', label: "Y Coordinate" }
+          ], 
+          flags: [
+            { flag: 'large-arc-flag', label: "Large Arc Flag"},
+            { flag: 'sweep-flag', label: "Sweep Flag"}
+          ]
+        },
+        {
+          command: 'Z', 
+          name: "Close Path"
+        }
+      ]
+    },
+  ],
+}
+
 const ShapeInputs = ({ shape }) => {
+  const {attributes, setAttributes, contextCommand, setContextCommand, coordinateDataContext, setcoordinateDataContext} = useContext(Context)
   const dynamicRefMap = useRef([]);
+  const commandReference = useRef('')
+  
   useEffect(() => {
     // Clear existing refs if shape changes
-    dynamicRefMap.current = [];
-    if (shapeConfig[shape]) {
-      shapeConfig[shape].forEach((_, index) => {
+    dynamicRefMap.current = {};
+    const config = advancedShapeConfig[shape] || basicShapeConfig[shape] || null
+    if (config) {
+      config.forEach((_, index) => {
         dynamicRefMap.current[index] = React.createRef();
       });
     }
   }, [shape]);
 
-  if (!shapeConfig[shape]) {
+  const handleCommandChange = (event) => {
+    setContextCommand(event.target.value)
+    commandReference.current = event.target.value //to use value immediately
+  }
+
+  const coordinateData = () => {// try to optimise later
+    //require input
+    const inputElements = document.querySelectorAll('form input')
+    let validity = [];
+    inputElements && 
+    (
+      inputElements.forEach((element, index) => {
+        validity[index] = element.checkValidity()
+      })
+    )
+    const element = inputElements[validity.indexOf(false)]
+    if(element){
+      element.reportValidity()
+    }
+    if(!validity.includes(false)){
+      setcoordinateDataContext((previousData) => [...previousData, ' ', contextCommand, Object.values(attributes).join(',')])
+      setAttributes({})
+    }
+  }
+
+  if (!basicShapeConfig[shape] && !advancedShapeConfig[shape]) {
     return null
   };
 
   return (
     <div>
-      {shape === 'PATH' && (
-        <h6>
-          M x y 
-          L x y
-          T x y
-          H x
-          V y 
-          C x1 y1, x2 y2, x y
-          S x2 y2, x y
-          Q x1 y1, x y
-          A rx ry x-axis-rotation large-arc-flag sweep-flag x y
-          Z
-        </h6>
-      )}
-      {shapeConfig[shape].map(({ parameter, label, type }, index) => 
+      {basicShapeConfig[shape]?.map(({ parameter, label }, index) => 
         <LabelInput
-          inputkey={parameter}
-          parameter={parameter}
-          label={label}
-          type={type}
-          inputReference={dynamicRefMap.current[index]}
+          inputkey={ parameter }
+          parameter={ parameter }
+          label={ label }
+          inputReference={ dynamicRefMap.current[index] }
         />
+      )}
+      {advancedShapeConfig[shape]?.map(({ attribute, label, commands, parameters }, index) => {
+        return (
+          <div key={attribute}>
+            <label htmlFor="shapeData">{label}</label>
+            <p id='shapeData'>{attribute}{coordinateDataContext}</p>
+            {commands && 
+              (
+                <div>
+                  <select className='mb-1' 
+                          name='' 
+                          id='commandSelection' 
+                          onChange={handleCommandChange} 
+                          value={contextCommand}>
+                            
+                    <option id={index} key={index} value='' disabled>Select a command</option>
+                    {commands.map(({command, name}) => 
+                      {
+                        return <option id={command} key={command} value={command}>{name}</option>
+                      }
+                    )}
+                  </select>
+                  <div>
+                    <fieldset name='parametersForCommand' id=''>
+                      {commands.filter(({command})=> command === commandReference.current)[0]?.parameters?.map(({ parameter, label }, index) => (
+                            <LabelInput
+                            inputkey={ parameter }
+                            parameter={ parameter }
+                            label={ label }
+                            inputReference={ null }
+                            />
+                        ))
+                        }
+                      <button type='button' id='commandsInput' onClick={coordinateData}>Add {commandReference.current}</button>
+                    </fieldset>
+                  </div>
+                </div>
+              )
+            }
+            {parameters && 
+              (
+                <div>
+                  <fieldset name='parametersForCommand' id=''>
+                    {parameters.map(({ parameter, label }, index) => 
+                      (
+                        <LabelInput
+                        inputkey={ parameter }
+                        parameter={ parameter }
+                        label={ label }
+                        inputReference={ null }
+                        />
+                      )
+                    )}
+                    <button type='button' id='parametersInput' onClick={coordinateData}>Add Coordinate</button>
+                  </fieldset>
+                </div>
+              )
+            }
+          </div>
+        )
+      }
       )}
     </div>
   );
