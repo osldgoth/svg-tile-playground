762037b3aa9a4db4b92dc4f87da48bd5750fe507
Stephen Brooks <osldgoth@gmail.com>
2024-06-20 21:21:22 -0400

    created defaultAttributes, format useContext, added condition for submit button

--- src/components/SVGForm.js
+++ src/components/SVGForm.js
@@ -4,33 +4,50 @@ import { Context } from "./SVGContext"
 import {v4 as uuidv4} from 'uuid'
 import SVGTile from './SVGTile'
 
+const defaultAttributes = {
+  "d":'',
+  "points":'',
+  "poly": {},
+  "path": {
+    "m":{},
+    "l":{},
+    "h":{},
+    "v":{},
+    "c":{},
+    "s":{},
+    "q":{},
+    "t":{},
+    "a":{},
+    "z":{}
+  },
+  "RECT": {}//...
+}
 
 const SVGForm = () => {
   const {shapeName, setShapeName, 
          currentSVG, setCurrentSVG, 
          allSVGs, setAllSVGs, 
          attributes, setAttributes, 
-         contextCommand, setContextCommand,
-         coordinateDataContext, setcoordinateDataContext} = useContext(Context) //currentSVG, setCurrentSVG, 
+         Command, setCommand,
+         coordinateData, setcoordinateData} = useContext(Context) //currentSVG, setCurrentSVG, 
   const shapes = ["Rectangle", "Circle", 'Ellipse', 'Line', 'Polyline', 'Polygon', 'Path']
 
   const handleFormSubmission = (event) => {
     event.preventDefault()
-  
-    setAllSVGs([{shapeName, attributes}, ...allSVGs]) //add new svg to beginning of array
+    setAllSVGs([{shapeName, attributes, coordinateData}, ...allSVGs]) //add new svg to beginning of array
       //reset state to defaults
     setShapeName("")
-    setAttributes({})
-    setcoordinateDataContext('')
-    setContextCommand('')
+    setAttributes(defaultAttributes)
+    setcoordinateData('')
+    setCommand('')
   }
 
   const handleShapeChange = (event) => {
     setShapeName(event.target.value)
       //reset state to defaults
-    setAttributes({})
-    setcoordinateDataContext('')
-    setContextCommand('')
+    setAttributes(defaultAttributes)
+    setcoordinateData('')
+    setCommand('')
   }
   
   return (
@@ -46,8 +63,10 @@ const SVGForm = () => {
       </select>
 
       <ShapeInputs shape={shapeName.toUpperCase()}/>
-      <SVGTile index={-1} shapeName={shapeName} attributes={attributes} /> {/* handleDelete={handleDelete} handleEdit={handleEdit} */}
-      <button type='submit' className='submit-svg-form my-3'>Create SVG</button>
+      <SVGTile index={-1} shapeName={shapeName} attributes={attributes} coordinateData={coordinateData}/> {/* handleDelete={handleDelete} handleEdit={handleEdit} */}
+      {shapeName &&
+        <button type='submit' className='submit-svg-form my-3'>Create SVG</button>
+      }
       
     </form>
       
