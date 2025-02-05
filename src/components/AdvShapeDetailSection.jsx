dd0c21e6d7bc2d589a2e588a566b6ea09f97f1e8
Stephen Brooks <osldgoth@gmail.com>
2025-02-05 00:01:07 -0500

    refactor: Ensure correct rendering of Path and Poly HTML

--- src/components/AdvShapeDetailSection.jsx
+++ src/components/AdvShapeDetailSection.jsx
@@ -1,13 +1,16 @@
 import PropTypes from 'prop-types';
 
-const AdvShapeDetailSection = ({attribute, label, sectionHTML, handleEditCoordLeft, handleEditCoordRight, editDeleteIcons, shape}) => {
+const AdvShapeDetailSection = ({attribute, label, pathHTML, polyHTML, handleEditCoordLeft, handleEditCoordRight, editDeleteIcons, shape}) => {
   return (
   <section name={label} value={attribute}>
     <h5>{label}</h5>
     <p id='shapeData'>
       {attribute}=
       <i className="d-none bi bi-box-arrow-in-left" onClick={() => handleEditCoordLeft(shape.toLowerCase())}></i>
-      &apos;{sectionHTML}&apos;
+      &apos;
+      { pathHTML.length ? pathHTML: null }
+      { polyHTML.length ? polyHTML: null }
+      &apos;
       <i className="d-none bi bi-box-arrow-in-right" onClick={() => handleEditCoordRight(shape.toLowerCase())}></i>
       {editDeleteIcons}
     </p>
@@ -18,12 +21,12 @@ const AdvShapeDetailSection = ({attribute, label, sectionHTML, handleEditCoordLe
 AdvShapeDetailSection.propTypes = {
   attribute: PropTypes.string,
   label: PropTypes.string,
-  sectionHTML: PropTypes.array,
+  pathHTML: PropTypes.array,
+  polyHTML: PropTypes.array,
   handleEditCoordLeft: PropTypes.func,
   handleEditCoordRight: PropTypes.func,
   editDeleteIcons: PropTypes.element,
   shape: PropTypes.string
-
 }
 
 export default AdvShapeDetailSection
