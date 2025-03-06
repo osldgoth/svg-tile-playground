b213cdf20e85209148519c5c850a57673e25b42f
Stephen Brooks <osldgoth@gmail.com>
2025-03-05 23:48:45 -0500

    refactor: move utility functions to separate file

--- src/components/SVGTileWrapper.jsx
+++ src/components/SVGTileWrapper.jsx
@@ -7,45 +7,23 @@ import { Context } from "./SVGContext"
 
 const SVGTileWrapper = () => {
   const {
-          setShapeName, 
-          allSVGs, setAllSVGs, 
-          setInputData, setProcessedData,
-          setBasic
+          allSVGs
         } = useContext(Context)
-  
-
-  const handleDelete = (event) =>{
-    //setAllSVGs
-    const indexAsInt = parseInt(event.target.getAttribute('data-index'))
-    setAllSVGs([...allSVGs.filter((_, currentIndex) => currentIndex !== indexAsInt )])
-  }
-
-  const handleEdit = (event) =>{
-    const indexAsInt = parseInt(event.target.getAttribute('data-index'))
-    const shapeName = allSVGs[indexAsInt].shapeName
-    const inputData = allSVGs[indexAsInt].basic ? {[shapeName]: allSVGs[indexAsInt].basic} : {}
-    const processedData = allSVGs[indexAsInt].processedData
-    setShapeName(shapeName)
-    //set inputs
-    setInputData(inputData)
-    setProcessedData(processedData)
-    setBasic('')
-    //then delete svg out of allsvgs
-    setAllSVGs([...allSVGs.filter((_, i) => i !== indexAsInt)])
-  }
 
   return (
-    <div className='container mt-3'>
+    <div className='container mt-3 px-0' style={{maxWidth: "95%"}}>
       <h3>SVG playground.</h3> 
-      <p>The following is not an exhaustive SVG builder, but rather a very basic and rudimentary one. 
-        I have created it simply for myself to become more familiar with SVG&apos;s and, along the way, React by building it.
-        For now I am sticking to absolute coordinates for simplicity.
+      <p>
+        This is not intended to be a comprehensive SVG builder, but rather a basic tool I created to familiarize myself with SVGs and React.
+        For simplicity, I&apos;m currently using absolute coordinates.
+        <br/>
+        Select a shape, enter values, save, edit and delete as needed.
       </p>
       <SVGForm/>
 
       {allSVGs.map((svgData, index)=> { 
         return (
-          <SVGTile key={index} index={index} svgData={svgData} handleDelete={handleDelete} handleEdit={handleEdit}/>
+          <SVGTile key={index} index={index} svgData={svgData}/>
         )
       })}
     </div>
